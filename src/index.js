fdff3a38ff4d156a651b86f2b8948432c195d58e
7516185Guo <gm990114@163.com>
2023-06-05 11:22:05 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./asstes/style/common.scss";
+// 先导入 antd 样式文件
+import "antd/dist/antd.css";
+// 再导入全局样式文件，防止样式覆盖
+import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
